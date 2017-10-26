91a1d686decd5cb3d2935b4ee08cc8e395adb433
Fabian <f.grunewald@student.rug.nl>
2017-10-26 14:09:03 +0200

    P3HT implementation

--- tests/compare.py
+++ tests/compare.py
@@ -4,7 +4,11 @@ ref=read_itp("PS100_Amap.itp")
 new=read_itp("test_PS.itp")
 ref_PEO=read_itp("PEO_Lee_37.itp")
 new_PEO=read_itp("test_PEO.itp")
-bonds, angles, constraints, dihedrals = [], [], [], []
+
+#ref_P3HT=read_itp("P3HT_Alessandri.itp")
+#new_P3HT=read_itp("test_P3HT.itp")
+
+bonds, angles, constraints, dihedrals, exclusions, virtual = [], [], [], [], [], []
 
 for i, j in zip(ref["bonds"], new["bonds"]):
     for a, b in zip(i,j):
@@ -30,7 +34,34 @@ for i, j in zip(sorted(ref_PEO["dihedrals"]), sorted(new_PEO["dihedrals"])):
      for a, b in zip(i,j):
          dihedrals += [a==b]
 
+for i, j in zip(ref_P3HT["bonds"], new_P3HT["bonds"]):
+    for a, b in zip(i,j):
+        bonds += [a==b]
+
+for i, j in zip(sorted(ref_P3HT["angles"]), sorted(new_P3HT["angles"])):
+     for a, b in zip(i,j):
+         angles += [a==b]
+
+for i, j in zip(sorted(ref_P3HT["dihedrals"]), sorted(new_P3HT["dihedrals"])):
+     for a, b in zip(i,j):
+         dihedrals += [a==b]
+
+for i, j in zip(sorted(ref_P3HT["virtual_sitesn"]), sorted(new_P3HT["virtual_sitesn"])):
+     for a, b in zip(i,j):
+         virtual += [a==b]
+
+for i, j in zip(sorted(ref_P3HT["exclusions"]), sorted(new_P3HT["exclusions"])):
+     for a, b in zip(i,j):
+         exclusions += [a==b]
+
+for i, j in zip(sorted(ref_P3HT["constraints"]), sorted(new_P3HT["constraints"])):
+     for a, b in zip(i,j):
+         constraints += [a==b]
+
 print("Bonds:", all(bonds))
 print("Angles:", all(angles))
 print("Constraints:", all(constraints))
 print("dihedrals:", all(dihedrals))
+print("virtuals:", all(dihedrals))
+print("exclusions:", all(dihedrals))
+
--- tests/test.sh
+++ tests/test.sh
@@ -1,5 +1,7 @@
 rm -f test_PS.itp
 rm -f test_PEO.itp
+rm -r test_P3HT.itp
+python3.5 ../polyply.py -n_mon 48 -itp ../monomer_itps/P3HT_Alessandri_mon.itp -o "test_P3HT.itp" -name P3HT48
 python3.5 ../polyply.py -n_mon 100 -itp ../monomer_itps/PS_Monticelli_mon.itp -o "test_PS.itp" -name PS100
 python3.5 ../polyply.py -n_mon 37 -itp ../monomer_itps/PEO_Lee_mon.itp -o "test_PEO.itp" -name PEO37
 python3.5 compare.py
