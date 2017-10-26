b709e71c25db19067279df77875112ff94101175
Fabian <f.grunewald@student.rug.nl>
2017-10-26 13:51:57 +0200

    added test script for PS&PEO

--- tests/compare.py
+++ tests/compare.py
@@ -1,8 +1,10 @@
 from Martini_PolyPly.itp_tool.itp_I import *
 
 ref=read_itp("PS100_Amap.itp")
-new=read_itp("test.itp")
-bonds, angles, constraints = [], [], []
+new=read_itp("test_PS.itp")
+ref_PEO=read_itp("PEO_Lee_37.itp")
+new_PEO=read_itp("test_PEO.itp")
+bonds, angles, constraints, dihedrals = [], [], [], []
 
 for i, j in zip(ref["bonds"], new["bonds"]):
     for a, b in zip(i,j):
@@ -16,6 +18,19 @@ for i, j in zip(sorted(ref["constraints"]), sorted(new["constraints"])):
      for a, b in zip(i,j):
          angles += [a==b]
 
+for i, j in zip(ref_PEO["bonds"], new_PEO["bonds"]):
+    for a, b in zip(i,j):
+        bonds += [a==b]
+
+for i, j in zip(sorted(ref_PEO["angles"]), sorted(new_PEO["angles"])):
+     for a, b in zip(i,j):
+         angles += [a==b]
+
+for i, j in zip(sorted(ref_PEO["dihedrals"]), sorted(new_PEO["dihedrals"])):
+     for a, b in zip(i,j):
+         dihedrals += [a==b]
+
 print("Bonds:", all(bonds))
 print("Angles:", all(angles))
 print("Constraints:", all(constraints))
+print("dihedrals:", all(dihedrals))
--- tests/test.sh
+++ tests/test.sh
@@ -1,3 +1,5 @@
-rm -f test.itp
-python3.5 ../polyply.py -n_mon 100 -itp ../monomer_itps/PS_Monticelli_mon.itp -o "test.itp" -name PS100
+rm -f test_PS.itp
+rm -f test_PEO.itp
+python3.5 ../polyply.py -n_mon 100 -itp ../monomer_itps/PS_Monticelli_mon.itp -o "test_PS.itp" -name PS100
+python3.5 ../polyply.py -n_mon 37 -itp ../monomer_itps/PEO_Lee_mon.itp -o "test_PEO.itp" -name PEO37
 python3.5 compare.py
